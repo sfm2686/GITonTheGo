0a845eecbb79da9de002ae108ecff10c6ba02eda
Sultan Mira <muro@Sultans-MacBook-Pro.local>
2017-12-13 17:22:06 -0500

    All stats should be working fine now, the numbers are not sorted, also might need to replace % with numbers instead for the most common commit word. Right now they are taking data from local files on the repo, one is a git log for ruby on rails and the other is the git log for this project, that needs to change to make it more dynamic in the future.

--- API/gotg-api.py
+++ API/gotg-api.py
@@ -1,10 +1,14 @@
+from __future__ import division
 import os
+import re
 import socket
-import clamd
+# import clamd
 import uuid
 import logging
+import json
+from collections import Counter
 from flask import Flask, request, redirect, url_for, jsonify
-from redis import Redis, RedisError
+# from redis import Redis, RedisError
 from werkzeug.utils import secure_filename
 
 
@@ -40,7 +44,7 @@ def init_user():
 @app.route(VERSION + '/init/dockerfile', methods=['POST'])
 def init_dockerfile():
 	# TODO - determine if clean enough for dockerfile
-	
+
 	# JSON object to be used for dockerfile
 	data = request.json
 	# Cleaning json object
@@ -52,3 +56,126 @@ def init_dockerfile():
 		data["framework"] = ""
 
 	return response('Success', 200, None, 'Successful submission of data for dockerfile!')
+
+# [GET] Stats 
+# @return - Succes: valid data processed || Fail: invalid data processed
+@app.route(VERSION + '/git/stats', methods=['GET'])
+def git_stats():
+	stats = {}
+	stats["table0"] = userPercentage()
+	stats["table1"] = dayPercentage()
+	stats["table2"] = timePercentage()
+	stats["table3"] = commitWord()
+	# TODO - most common commit word
+
+	# return response('Success', 200, jsonify(stats), 'Successful stats processed')
+	return jsonify(stats)
+
+###########################################
+## STATS PROCESSING METHODS
+###########################################
+def userPercentage():
+	file = open("git-log2.txt", "r")
+
+	authors = []
+
+	for line in file:
+		if 'Author' in line:
+			authors.append(line[8:])
+
+	authorsDict = {}
+
+	for author in authors:
+		if author in authorsDict:
+			authorsDict[author] += 1
+		else:
+			authorsDict[author] = 1
+
+	result = {}
+
+	for author in dict(Counter(authorsDict).most_common(10)):
+		result[author] = round(((authorsDict[author]/len(authors)) * 100), 2)
+
+	app.logger.info(result)
+
+	return result
+
+def dayPercentage():
+	file = open("git-log2.txt", "r")
+
+	days = []
+
+	for line in file:
+		if 'Date:   ' in line:
+			days.append(line[8:11])
+
+	daysDict = {}
+
+	for day in days:
+		if day in daysDict:
+			daysDict[day] += 1
+		else:
+			daysDict[day] = 1
+
+	result = {}
+	for day in daysDict:
+		result[day] = round(((daysDict[day]/len(days)) * 100), 2)
+
+	return result
+
+def timePercentage():
+	file = open("git-log.txt", "r")
+
+	times = []
+
+	for line in file:
+		if 'Date:   ' in line:
+			times.append(line[19:21].replace(":", ""))
+
+	timesDict = {}
+
+	for time in times:
+		if time in timesDict:
+			timesDict[time] += 1
+		else:
+			timesDict[time] = 1
+
+	result = {}
+
+	for time in dict(Counter(timesDict).most_common(10)):
+		result[time] = round(((timesDict[time]/len(times)) * 100), 2)
+
+	return result
+
+def commitWord():
+	file = open("git-log2.txt", "r")
+
+	proc = False
+	words = []
+
+	for line in file:
+		if line.startswith("Date:   "):
+			proc = True
+			continue
+		elif line.startswith("commit"):
+			proc = False
+		if proc:
+			words.extend(re.split("[^a-zA-Z]", line))
+
+	while '' in words:
+		words.remove('')
+
+	wordsDict = {}
+
+	for word in words:
+		if word in wordsDict:
+			wordsDict[word] += 1
+		else:
+			wordsDict[word] = 1
+
+	result = {}
+
+	for word in dict(Counter(wordsDict).most_common(10)):
+		result[word] = round(((wordsDict[word]/len(words)) * 100), 2)
+
+	return result
--- UI/js/statsPage.js
+++ UI/js/statsPage.js
@@ -1,3 +1,33 @@
+$.getJSON("http://127.0.0.1:5000/api/v1/git/stats",
+	function(data) {
+
+		// Populating table 0 (user percentage)
+		var currentTable = data["table0"];
+		var keys = Object.keys(currentTable);
+		popTable("stat0Table", currentTable, keys);
+
+		// Populating table 1 (day percentage)
+		currentTable = data["table1"];
+		keys = Object.keys(currentTable);
+		popTable("stat1Table", currentTable, keys);
+		
+		// Populating table 2 (day percentage)
+		currentTable = data["table2"];
+		keys = Object.keys(currentTable);
+		popTable("stat2Table", currentTable, keys);
+
+		// Populating table 3 (common commit word)
+		currentTable = data["table3"];
+		keys = Object.keys(currentTable);
+		popTable("stat3Table", currentTable, keys);
+	});
+
+function popTable(tableID, data, keys) {
+	for (var i = keys.length - 1; i >= 0; i --) {
+		$("#" + tableID).append("<tr><td>" + keys[i] + "</td><td>" + data[keys[i]] + "%</td></tr>");
+	}
+}
+
 function goToShell() {
   // flask api calls happen here
 }
